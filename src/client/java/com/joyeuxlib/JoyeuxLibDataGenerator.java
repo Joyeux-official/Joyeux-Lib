d102776cc37ff995377a69c4b6e7f75e8c41e2eb
Loqor <isaiahcrace@gmail.com>
2025-07-16 18:53:26 -0500

    Fix the english provider

--- src/client/java/com/joyeuxlib/JoyeuxLibDataGenerator.java
+++ src/client/java/com/joyeuxlib/JoyeuxLibDataGenerator.java
@@ -6,6 +6,7 @@ import com.joyeuxlib.datagenproviders.JoyeuxLibRecipeProvider;
 import com.joyeuxlib.datagenproviders.JoyeuxLibSoundProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import org.jetbrains.annotations.ApiStatus;
 
 /**
@@ -16,6 +17,13 @@ public class JoyeuxLibDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		pack.addProvider(JoyeuxLibEnglishLangProvider::new);
+		pack.addProvider(this::englishLibProvider);
+	}
+
+	public JoyeuxLibEnglishLangProvider englishLibProvider(FabricDataOutput output) {
+		JoyeuxLibEnglishLangProvider joyENUSLang = new JoyeuxLibEnglishLangProvider(output);
+		joyENUSLang.addTranslation("block.joyeux-lib.joy_block","Joy");
+		joyENUSLang.addTranslation("block.joyeux-lib.koy_block","Koy");
+		return joyENUSLang;
 	}
 }
