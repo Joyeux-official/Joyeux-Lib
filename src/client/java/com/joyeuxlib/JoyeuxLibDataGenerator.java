b3a9e7b7501eb8c091d4995917acfd6db07e643b
Addi3 <122154427+Addi3@users.noreply.github.com>
2025-07-16 07:38:16 +0100

    now split client and common sources

---
+++ src/client/java/com/joyeuxlib/JoyeuxLibDataGenerator.java
@@ -0,0 +1,11 @@
+package com.joyeuxlib;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JoyeuxLibDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
