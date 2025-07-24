8bca56b45b5512943fa9642eb69daf9d70740a26
Addi3 <122154427+Addi3@users.noreply.github.com>
2025-07-24 22:47:57 +0100

    feat: difhandmodelitem for hand held mixins

---
+++ src/main/java/com/joyeuxlib/core/items/DifferingHandModelItem.java
@@ -0,0 +1,9 @@
+package com.joyeuxlib.core.items;
+
+/**
+ * @author Loqor
+ * @license GNU General Public License v3.0
+ * This is for items that have different models for when in the inventory/ground and in the hand.
+ */
+public interface DifferingHandModelItem {
+}
