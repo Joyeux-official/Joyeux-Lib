b5945382cb6c605f3837af2a30ac6f2749eb785f
Capozi <capozi.devv@gmail.com>
2025-09-01 16:46:03 -0400

    Made structures able to be big af

--- src/main/java/com/joyeuxlib/JoyeuxLib.java
+++ src/main/java/com/joyeuxlib/JoyeuxLib.java
@@ -2,12 +2,16 @@ package com.joyeuxlib;
 
 import com.joyeuxlib.core.JoyeuxLibBlocks;
 import com.joyeuxlib.core.JoyeuxLibSounds;
+import com.joyeuxlib.core.worldgen.ExpandedJigsawStructure;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.structure.StructureType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +19,12 @@ public class JoyeuxLib implements ModInitializer {
 	public static final String MOD_ID = "joyeux-lib";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static Block BoxBlock;
-
+    public static final StructureType<ExpandedJigsawStructure> EXPANDED_JIGSAW =
+            Registry.register(
+                    Registries.STRUCTURE_TYPE,
+                    new Identifier(MOD_ID, "expanded_jigsaw"),
+                    () -> ExpandedJigsawStructure.CODEC
+            );
 	public static Identifier id(String path) {
 		return new Identifier(MOD_ID, path);
 	}
